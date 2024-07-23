e3d648d367bf84b67e4f1c1fd1af604bba5bf870
asaringo99 <ka99tns89@gmail.com>
2024-07-24 04:34:57 +0900

    kebabcase

--- medium/296-Permutation/index.ts
+++ medium/296-Permutation/index.ts
@@ -20,7 +20,6 @@ type Permutation<T> = any
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../../utils'
-
 type cases = [
   Expect<Equal<Permutation<'A'>, ['A']>>,
   Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
